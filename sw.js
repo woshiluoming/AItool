071d849428e1f7bb73139f6787829f2b4264eb07
luoming214001 <luoming.214001@sieyuan.com>
2026-08-12 15:48:36 +0800

    修改广告

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11559510
+    "zoneId": 11559591
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
